e5006fa672096b31def8c0dd9a7bad5eb34ff35e
Camilo Almeida <camiloalmeida22@gmail.com>
2024-06-21 21:15:44 -0300

    projeto criado

---
+++ src/main/java/com/sn/games_list/GamesListApplication.java
@@ -0,0 +1,13 @@
+package com.sn.games_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesListApplication.class, args);
+	}
+
+}
